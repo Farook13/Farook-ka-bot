38c3b1de2ec1ef305acf1fafc5dda94854d82d9f
Farook13 <umarulfarook787@gmail.com>
2024-10-05 13:10:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ class evamaria(Client):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['None'])
-API_HASH = environ['None']
+API_ID = int(environ['12618934'])
+API_HASH = environ['environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')']
 BOT_TOKEN = environ['7955983025:AAHdF6kZS4qcN7FURwMCp-_cuYa-9zFB7IE']
 
 # Bot settings
