51e334e585e419593016973b9a873348b66f830e
Farook13 <umarulfarook787@gmail.com>
2024-10-06 14:55:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ZOROB:ZOROB@cluster0.uoqes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Elsa")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://ZOROB:ZOROB@cluster0.uoqes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
