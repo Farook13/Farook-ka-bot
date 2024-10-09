c09e47d7cc4db8bbf18a69fb1d0086fc49177a07
Farook13 <umarulfarook787@gmail.com>
2024-10-09 14:12:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,7 +77,7 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://umarulfarook1313:Farook13@cluster0.jocxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Farubi")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://umarulfarook1313:Farook13@cluster0.lm6hf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://umarulfarook1313:Farook13@cluster0.jocxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
