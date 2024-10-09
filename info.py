f2c5e8884a93c03047e69cc6a1ba0283337b3a8f
Farook13 <umarulfarook787@gmail.com>
2024-10-09 14:24:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://umarulfarook1313:Farook13@cluster0.jocxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Farubi")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://umarulfarook1313:Farook13@cluster0.jocxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
